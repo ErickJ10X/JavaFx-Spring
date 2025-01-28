96e25d6de91bedf63262899d49c587420eeec7cb
erick <erickjo10x@gmail.com>
2025-01-28 14:53:12 +0100

    remove import

--- src/main/java/com/prog/fx/scenes/MenuScene.java
+++ src/main/java/com/prog/fx/scenes/MenuScene.java
@@ -1,7 +1,6 @@
 package com.prog.fx.scenes;
 
 import com.prog.fx.FxApplication;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
