6275e1e1d134dca16ff0073b933f51ad2d1eb141
erick <erickjo10x@gmail.com>
2025-01-28 14:54:42 +0100

    remove import

--- src/main/java/com/prog/fx/producto/productoController.java
+++ src/main/java/com/prog/fx/producto/productoController.java
@@ -1,11 +1,7 @@
 package com.prog.fx.producto;
 
-import javafx.fxml.Initializable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.net.URL;
-import java.util.ResourceBundle;
 
 @Component
 public class productoController {
