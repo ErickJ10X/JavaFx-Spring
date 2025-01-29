327140214a2a8243f21b6e2de106c15e94213c48
erick <erickjo10x@gmail.com>
2025-01-29 14:55:45 +0100

    add ListaProductsScene class

---
+++ src/main/java/com/prog/fx/scenes/ListProductsScene.java
@@ -0,0 +1,41 @@
+package com.prog.fx.scenes;
+
+import com.prog.fx.FxApplication;
+import com.prog.fx.producto.Producto;
+import com.prog.fx.producto.ProductoService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class ListProductsScene {
+
+    private Scene scene;
+
+    public ListProductsScene(Stage stage) {
+        VBox layout = new VBox();
+        ListView<String> listView = new ListView<>();
+        Button backButton = new Button("Back to Menu");
+        backButton.setOnAction(e -> stage.setScene(new MainMenuScene(stage).getScene()));
+
+        // Fetch products and add to list view
+        ConfigurableApplicationContext context = FxApplication.context;
+        ProductoService productoService = context.getBean(ProductoService.class);
+        ObservableList<String> items = FXCollections.observableArrayList();
+        for (Producto producto : productoService.findAll()) {
+            items.add(producto.getNombre() + " - " + producto.getDescripcion());
+        }
+        listView.setItems(items);
+
+        layout.getChildren().addAll(listView, backButton);
+        scene = new Scene(layout, 300, 200);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
