dadbb66980ec3e32038b516495b9856cad519052
erick <erickjo10x@gmail.com>
2025-01-29 13:23:13 +0100

    modify repository

--- src/main/java/com/prog/fx/producto/ProductoRepository.java
+++ src/main/java/com/prog/fx/producto/ProductoRepository.java
@@ -2,5 +2,5 @@ package com.prog.fx.producto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductoRepository extends JpaRepository<Productos, Long> {
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
 }
